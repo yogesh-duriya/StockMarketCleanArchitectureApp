bf2b9627d7dec3456a4d7aa81a08ea2eb07909f8
Yogesh Duriya <yogesh.duriya@sworks.io>
2022-04-15 01:01:04 +0530

    Initial structure for compose

--- app/src/androidTest/java/com/ydcoding/stockmarket/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ydcoding/stockmarket/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.ydcoding.stockmarketcleanarchitectureapp
+package com.ydcoding.stockmarket
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
